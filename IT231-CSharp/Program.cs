1736d35bc564a3bd7bcef961cce9d306c0f6f156
Michaela <143545267+mpartsch01@users.noreply.github.com>
2024-12-05 13:17:46 -0500

    added csharp file
    trying to track the files

---
+++ IT231-CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IT231_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
