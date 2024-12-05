a752bfcb4e29c43e6e84a66defa1ff1f05b28afd
Michaela <143545267+mpartsch01@users.noreply.github.com>
2024-12-05 15:32:59 -0500

    add math

--- IT231-CSharp/Program.cs
+++ IT231-CSharp/Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            DoMath(3, 4);
+
+        }
+
+        public static void DoMath(int x, int y)
+        {
+
+            Console.WriteLine("Enter your first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You've entered {x}");
+            Console.WriteLine("Enter your second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You've entered {y}");
+            Console.WriteLine("And the sum of these two numbers is: ");
+            Console.Write(x + y);
         }
     }
 }
